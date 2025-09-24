bbf4bfc5d5075527717cf6ff7af0d1e8a0489ff1
BINHTRAN\binh0 <22115053122103@sv.ute.udn.vn>
2025-09-25 03:55:54 +0700

    feat(ui): layout Welcome screen

---
+++ app/src/main/java/com/example/android_login/WelcomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.android_login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    Button btnCreateAccount103, btnLogin103;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        btnCreateAccount103 = findViewById(R.id.btnCreateAccount103);
+        btnLogin103 = findViewById(R.id.btnLogin103);
+
+//        btnCreateAccount103.setOnClickListener(v ->
+//                startActivity(new Intent(this, CreateAccountActivity.class)));
+//        btnLogin103.setOnClickListener(v ->
+//                startActivity(new Intent(this, CreateAccountActivity.class)));
+       }
+}
